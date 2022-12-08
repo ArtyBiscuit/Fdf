3c7a7138e8085b89685c24b73401f8a7e88bbb31
ArtyBiscuit <forgeard.arthur@hotmail.com>
2022-12-08 14:57:24 +0100

    Bug Fix DDA_algo

--- dda_alogo.c
+++ dda_alogo.c
@@ -11,43 +11,16 @@
 /* ************************************************************************** */
 #include "fdf.h"
 
-static void	float_swap(float *arg1, float *arg2)
-{
-	float	tmp;
-
-	tmp = *arg1;
-	*arg1 = *arg2;
-	*arg2 = tmp;
-}
-
-static void	cheak_swap(t_point *start, t_point *end)
-{
-	if (start->x < end->x)
-	{
-		float_swap(&start->x, &end->x);
-		float_swap(&start->y, &end->y);
-	}
-	if (start->x > end->x)
-	{
-		float_swap(&start->x, &end->x);
-		float_swap(&start->y, &end->y);
-	}
-}
-
 int	**dda_algorithme(t_point *start, t_point *end/*, int **matric*/)
 {
 	float	d_y;
 	float	d_x;
-	float	steps;
+	int		steps;
 	int		cmp;
 
-	cheak_swap(start, end);
 	d_y = end->y - start->y;
 	d_x = end->x - start->x;
-	if (d_x >= d_y)
-		steps = d_x;
-	else
-		steps = d_y;
+	steps = (int)fmax(fabs(d_x), fabs(d_y));
 	d_x = d_x / steps;
 	d_y = d_y / steps;
 	cmp = 1;
